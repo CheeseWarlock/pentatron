7a320a54305213798937684f8f1c599ee9137a8c
Joe <joe.cardwell@gmail.com>
2025-06-13 10:49:15 -0400

    More optimization

--- src/components/NoteLightGrid.tsx
+++ src/components/NoteLightGrid.tsx
@@ -3,21 +3,29 @@ import { useMemo } from "react";
 import PlayerGridColumn from "./PlayerGridColumn";
 
 function NoteLightGrid({ noteGrid, activeColumn, onNoteGridUpdate }: { noteGrid: boolean[][], activeColumn: number, onNoteGridUpdate: (row: number, col: number) => void }) {
-  const noteGridElements = useMemo(() => {
-    return (noteGrid.map((col, colIndex) => (
-      <PlayerGridColumn
+  const memoizedList = noteGrid.map((col, colIndex) => {
+    const isCurrentColumn = activeColumn === colIndex;
+    
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useMemo(() => {
+      return <PlayerGridColumn
         key={`col-${colIndex}`}
         column={colIndex}
         activeNotes={col}
-        isCurrentColumn={activeColumn === colIndex}
+        isCurrentColumn={isCurrentColumn}
         onNoteGridUpdate={onNoteGridUpdate} />
-    )))
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  , [isCurrentColumn, onNoteGridUpdate])
 
-  }, [noteGrid, activeColumn, onNoteGridUpdate]);
+  }
+
+);
+
   
   return (
     <>
-      {noteGridElements}
+      {memoizedList}
     </>
   )
 }
